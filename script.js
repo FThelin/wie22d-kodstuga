867c54e0dd8203165fd29b6456deeea4ad97293b
FThelin <fredrikthelin82@gmail.com>
2022-09-28 10:02:42 +0200

    kodstuga2

--- script.js
+++ script.js
@@ -1,61 +1,105 @@
-//DATA TYPES
+// //DATA TYPES
 
-//Primitive Types
-const string = "This is a string"; //string
-const number = 10; //number
-const bool = true; //boolean
-const und = undefined; //undefined
-const nil = null; //null
+// //Primitive Types
+// const string = "This is a string"; //string
+// const number = 10; //number
+// const bool = true; //boolean
+// const und = undefined; //undefined
+// const nil = null; //null
 
-//Object types
-const list = [1, "2", 3, 4, 5, 6]; //Array (lista)'
+// //Object types
+// const list = [1, "2", 3, 4, 5, 6]; //Array (lista)'
+// //Object
+// const person = {
+//   firstName: "Fredrik",
+//   lastName: "Thelin",
+// };
+
+// //VARIABLES
+// // var myVariable;
+// let myVariable2;
+// const myVariable3 = "Thelin";
+// myVariable2 = "Björn";
+
+// //CONSOLE
+// //console.log(myVariable2);
+
+// //ALERT/PROMT
+// // alert("This is an alert");
+// // const number1 = prompt("Säg ett nummer");
+// // const number2 = prompt("Säg ett till nummer");
+// //console.log(parseInt(number1) + parseInt(number2));
+// //MATH OPERATORS
+// //const x = 16 + 3;
+// // console.log(x);
+
+// //LOGICAL OPERATORS
+// const x = 1;
+// const y = 2;
+// const z = 3;
+// const m = 4;
+
+// //const age = prompt("Hur gammal är du?");
+
+// // if (age <= 18) {
+// //   console.log("Du är inte ens myndig");
+// // } else if (age > 18 && age < 39) {
+// //   console.log("Du är alldeles lagom");
+// // } else {
+// //   console.log("Du är lastgammal");
+// // }
+
+// //STRING CONCAT
+// const firstName = "Fredrik";
+// const lastName = "Thelin";
+// const age = 40;
+
+// // console.log(
+// //   "Du heter " + firstName + " " + lastName + ", och du är " + age + " år"
+// // );
+
+// console.log(`Du heter ${firstName} ${lastName} och du är ${age} år`);
+
+//KODSTUGA 2
+//Array
+//const myArray = [1, 2, 3, 4, 5, 6, 7, 8];
+// myArray.pop();
+// console.log(myArray);
+
 //Object
-const person = {
-  firstName: "Fredrik",
-  lastName: "Thelin",
-};
+// const myObject = {
+//   firstName: "Fredrik",
+//   lastName: "Thelin",
+//   age: 40,
+//   city: "Vadstena",
+//   isAwesome: true,
+// };
 
-//VARIABLES
-// var myVariable;
-let myVariable2;
-const myVariable3 = "Thelin";
-myVariable2 = "Björn";
+// console.log(myObject.firstName);
 
-//CONSOLE
-//console.log(myVariable2);
+// const heading = document.getElementsByTagName("h1");
+// console.log(heading);
 
-//ALERT/PROMT
-// alert("This is an alert");
-// const number1 = prompt("Säg ett nummer");
-// const number2 = prompt("Säg ett till nummer");
-//console.log(parseInt(number1) + parseInt(number2));
-//MATH OPERATORS
-//const x = 16 + 3;
-// console.log(x);
+// const myArray = [1, 2, 3, 4, 5, 6, 7, 8];
 
-//LOGICAL OPERATORS
-const x = 1;
-const y = 2;
-const z = 3;
-const m = 4;
-
-//const age = prompt("Hur gammal är du?");
-
-// if (age <= 18) {
-//   console.log("Du är inte ens myndig");
-// } else if (age > 18 && age < 39) {
-//   console.log("Du är alldeles lagom");
-// } else {
-//   console.log("Du är lastgammal");
+//Js for loop
+// for (let i = 0; i <= myArray.length; i++) {
+//   console.log(i);
 // }
 
-//STRING CONCAT
-const firstName = "Fredrik";
-const lastName = "Thelin";
-const age = 40;
+//JS for of loop
+// for (const number of myArray) {
+//   console.log(number);
+// }
+// const doubledNumber = double(8);
+// console.log(doubledNumber);
 
-// console.log(
-//   "Du heter " + firstName + " " + lastName + ", och du är " + age + " år"
-// );
+// function double(num) {
+//   return num * 2;
+// }
 
-console.log(`Du heter ${firstName} ${lastName} och du är ${age} år`);
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", function () {
+  console.log("Du tryckte på knappen");
+});
